291deea9564126eef237c60f6f4330f84f6a929f
Unknown <lucas.addisi@gmail.com>
2018-01-25 11:22:00 -0300

    NodeLogRecords added to model

--- mapView/models.py
+++ mapView/models.py
@@ -28,6 +28,8 @@ class Location(models.Model):
     location_lat = models.DecimalField(max_digits=11, decimal_places = 7)
     location_lon = models.DecimalField(max_digits=11, decimal_places = 7)
 
-# class Node_status_records(models.Model):
-#     node = models.ForeignKey('Node', on_delete=models.CASCADE)
-#     record_date = models.DateTimeField(auto_now_add=True)
+class NodeLogRecords(models.Model):
+     node = models.ForeignKey('Node', on_delete=models.CASCADE)
+     record_date = models.DateTimeField(auto_now_add=True)
+     record_electric_current = models.DecimalField(max_digits = 5, decimal_places=3)
+     record_temperature = models.DecimalField(max_digits = 5, decimal_places = 2)
