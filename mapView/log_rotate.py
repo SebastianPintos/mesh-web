fa002a86fc9b9bc31815ab35b93dcb9bba26d8f9
Lucas Addisi <lucas.addisi@gmail.com>
2018-01-31 11:41:06 -0300

    now, system logging in csv format

--- mapView/log_rotate.py
+++ mapView/log_rotate.py
@@ -1,19 +1,22 @@
 import logging
 import time
+import csv
 
 from logging.handlers import TimedRotatingFileHandler
 
 class Logger:
-	
+
 	def __init__(self, path, periodicity):
 		self.logger=logging.getLogger("Changeme")
 		self.logger.setLevel(logging.INFO)
-		
+
 		handler = TimedRotatingFileHandler(path, when=periodicity, interval=1, backupCount=5)
-		
+
 		self.logger.addHandler(handler)
 
-		
-	def log(self, string):
+	def write(self, string):
 		self.logger.info(string)
 
+	def log_csv(self, string_list):
+		writer = csv.writer(self, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+		writer.writerow(string_list)
