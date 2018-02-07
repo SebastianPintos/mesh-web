307f0caf0ead8eb6d5008c07c73082cfe6fa9c65
Agus Alexander <agusalexander@hotmail.com>
2018-02-07 20:39:04 -0300

    logging

--- scripts/python/Logging.py
+++ scripts/python/Logging.py
@@ -1,8 +1,9 @@
-import socket, threading
+import socket, time
+print ()
 
 UDP_IP = "0.0.0.0"
 
-UDP_PORT = 5005
+UDP_PORT = 5007
 
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -11,4 +12,6 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message:", data)
+    with open("data.log", "a") as myfile:
+        myfile.write(time.strftime("%H:%M:%S - %d/%m/%Y")+" "+data+"\n")
+    print(time.strftime("%H:%M:%S - %d/%m/%Y")+" - "+data+"\n")
