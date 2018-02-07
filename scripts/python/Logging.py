590c2ea579399c703a08a9847537cdeab2fcd39b
Agus Alexander <agusalexander@hotmail.com>
2018-02-07 20:53:05 -0300

    Minor fix

--- scripts/python/Logging.py
+++ scripts/python/Logging.py
@@ -9,9 +9,10 @@ UDP_PORT = 5007
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-
+with open("data.log", "a") as myfile:
+        myfile.write(time.strftime("%H:%M:%S - %d/%m/%Y")+" - SERVER STARTED   \n")
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     with open("data.log", "a") as myfile:
-        myfile.write(time.strftime("%H:%M:%S - %d/%m/%Y")+" "+data+"\n")
+        myfile.write(time.strftime("%H:%M:%S - %d/%m/%Y")+" - "+data+"\n")
     print(time.strftime("%H:%M:%S - %d/%m/%Y")+" - "+data+"\n")
