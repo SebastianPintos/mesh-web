5f3aee82e10eb042fde25753caaba5ea4c68b430
Agus Alexander <agusalexander@hotmail.com>
2018-02-07 22:10:25 -0300

    now Logging.py can recieve listening port as argument

--- scripts/python/Logging.py
+++ scripts/python/Logging.py
@@ -1,18 +1,23 @@
-import socket, time
-print ()
+import socket, time , sys
 
+logName="data.log"
+def log(message):
+    timeStamp=time.strftime("%H:%M:%S - %d/%m/%Y")+" - "
+    with open("data.log", "a") as myfile:
+        myfile.write(timeStamp+message+"\n")
+    print(timeStamp+message+"\n")
 UDP_IP = "0.0.0.0"
-
-UDP_PORT = 5007
-
-
+UDP_PORT = 5009
+if(len(sys.argv)==2):
+	UDP_PORT = int(sys.argv[1])
+log("Logging initializing...\n")
+log("Binding to "+str(UDP_IP)+":"+str(UDP_PORT))
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-with open("data.log", "a") as myfile:
-        myfile.write(time.strftime("%H:%M:%S - %d/%m/%Y")+" - SERVER STARTED   \n")
+
+log("SERVER STARTED...");
+
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    with open("data.log", "a") as myfile:
-        myfile.write(time.strftime("%H:%M:%S - %d/%m/%Y")+" - "+data+"\n")
-    print(time.strftime("%H:%M:%S - %d/%m/%Y")+" - "+data+"\n")
+    log(data)
