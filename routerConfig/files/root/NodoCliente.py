655fdc3a6096e73aac622a02c7217cd3c2035d01
Agus Alexander <agusalexander@hotmail.com>
2018-02-16 16:54:23 -0300

    arduino reboot fix added and files fixed for custom firm

--- routerConfig/files/root/NodoCliente.py
+++ routerConfig/files/root/NodoCliente.py
@@ -1,32 +1,33 @@
 from socket import *
 import time,traceback
+import json
+import datetime
 
+IP_LOCAL_NODE = '10.10.5.4'
 UDP_PORT_Arduino = 8888
-IP_NODO_MASTER= '192.168.2.255'
+IP_NODO_MASTER= '10.10.5.5'
 PUERTO_NODO_MASTER= 5005
 
+def add_data(json_object):
+    json_object['ip'] = get_mesh_ip()
+    json_object['timestamp'] = get_timestamp()
+
+def get_timestamp():
+    ts = time.time()
+    return datetime.datetime.fromtimestamp(ts).isoformat()
+
+def get_mesh_ip():
+    return IP_LOCAL_NODE
+
 def enviarUDP(IP,port,message):
-    print("\n/////// Enviar UDP ///////\n")
-    print("Creando Socket...")
-    try:
-        sock = socket(AF_INET, SOCK_DGRAM)
-    except:
-        print("error")
-        print("\n/////// FIN Enviar UDP ///////")
-        return
-    print("listo")
-    print("Enviando Mensaje...")
 
     try:
+        sock = socket(AF_INET, SOCK_DGRAM)
         sock.sendto(message, (IP, port))
     except:
         print("error")
         print(traceback.format_exc())
-        print("\n/////// FIN Enviar UDP ///////")
         return
-    print("listo")
-    print("\n/////// FIN Enviar UDP ///////")
-
 
 
 def escucharMensajesArduino():
@@ -34,12 +35,12 @@ def escucharMensajesArduino():
 
     try:
         sock = socket(AF_INET, SOCK_DGRAM)
-        sock.bind(("0.0.0.0", UDP_PORT_Arduino))
+        sock.bind(("0.0.0.0", UDP_PORT_Arduino))#Sacar a archivo properties o como variable global
     except:
         print("error")
         print(traceback.format_exc())
         print("Reintentando...\n")
-        time.sleep(3)
+        time.sleep(5)
         escucharMensajesArduino()# Reintento Abrir socket
         return
 
@@ -47,18 +48,22 @@ def escucharMensajesArduino():
 
     while True:
         print("\n/////Escuchando Arduino/////\n")
-        print("...")
         try:
             data,addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
         except:
             print("error")
             escucharMensajesArduino()
             return
-        print(" ok\n")
-        print(data)
+        # json decode
+        json_data = json.loads(data.decode('utf-8'))
+        # Ponerla la ip y el timestamp
+        add_data(json_data)
+        # Volver a codificar
+        to_send = json.dumps(json_data)
+        print(to_send)
         print("\nEnviando a NodoMaster...")
         print("\n/////FIN Escuchando Arduino/////")
-        enviarUDP(IP_NODO_MASTER,PUERTO_NODO_MASTER,data)
+        enviarUDP(IP_NODO_MASTER,PUERTO_NODO_MASTER,to_send)
 
 print("\n//////////NODO CLIENTE/////////\n")
 escucharMensajesArduino()
