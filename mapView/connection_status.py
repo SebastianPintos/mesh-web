d7326317ed132d53da8c7348a425ebe53f5711e6
lucas <lucas.addisi@gmail.com>
2018-08-13 11:01:55 -0300

    changes in connection_status

--- mapView/connection_status.py
+++ mapView/connection_status.py
@@ -1,6 +1,6 @@
 import subprocess
 import schedule, time, json
-from mail_sender import MailSender
+from mapView.mail_sender import MailSender
 
 def detect_connection(ip_address, intents):
     procces = subprocess.run(['ping', '-c', str(intents), ip_address], shell=False, stdout=subprocess.PIPE)
@@ -10,6 +10,17 @@ def print_traceroute(ip_address):
     procces = subprocess.run(['traceroute', ip_address], shell=False, stdout=subprocess.PIPE)
     return str(procces.stdout)
 
+def is_time_exceeded(node ,exceeded_time):
+    last_register = NodeLogCurrentRecords.objects.filter(record_node = node).last().record_date
+    actual_date = datetime.now()
+    
+    actual_date = actual_date.replace(tzinfo=utc)
+    last_register = last_register.replace(tzinfo=utc)   #FIXME Si confiaramos en los estados, esto debería preguntar el estado, y si es violeta, directamente notificarlo
+    
+    difference = last_register - node_date
+    
+    print(difference)
+
 def main():
     mail_sender = MailSender("../email_settings.ini")
     with open('config.json') as confi:
@@ -17,14 +28,14 @@ def main():
     
     print(data['master_node']['ip']) #TODO Reemplazar Data en detect_connection
     
-    if(not detect_connection("192.168.0.10", 2)):
-        mail_sender.send_mail("lucas.addisi@gmail.com", "Test", print_traceroute("192.168.0.10"))
+    if(not detect_connection("192.168.3.2", 2)):
+        mail_sender.send_mail("lucas.addisi@gmail.com", "Test", print_traceroute("www.google.com"))#TODO Reemplazar Data en detect_connection
     else:
         print("Se detectó conectividad")
 
 
 def scan_connectivity_job():
-    #schedule.every().day.at("00:00").do(main)
+    schedule.every().day.at("12:33c").do(main)
     while True:
         schedule.run_pending()
         time.sleep(1)
--- webMesh/settings.py
+++ webMesh/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'webMesh.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#        }
-#}
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'web_mesh',
-        'USER': 'root',
-        'PASSWORD': 'ungs123!',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
+
+# 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'web_mesh',
+#         'USER': 'root',
+#         'PASSWORD': 'ungs123!',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
